537af985510d739d4de6cbbef6aff55ad6d668bd
iamwairimu <kmnleah@gmail.com>
2024-10-18 00:57:46 +0300

    added dev dependancies due to error

--- src/Pages/FeedbackPg.js
+++ src/Pages/FeedbackPg.js
@@ -7,7 +7,7 @@ function FeedbackPg() {
   return (
     <div className="back">
       <Navbar></Navbar>
-      <div>Feedback Page</div>
+      <div>Feedback Page checking on how to commit</div>
     </div>
   );
 }
